d0c2e277ecd538f93b7a435f7b6386035a9767a6
Manuel João <134264511+manuel-joao2023@users.noreply.github.com>
2023-12-22 12:06:34 +0100

    Delete JavaSEApp/src/Files directory

--- JavaSEApp/src/Files/Estudante.cs
+++
@@ -1,6 +0,0 @@
-public class Estudante {
-	public int Id { get; set; }
-	public string nome { get; set; }
-	public int idade { get; set; }
-	public Curso curso { get; set; }
-}
--- JavaSEApp/src/Files/Livro.cs
+++
@@ -1,5 +0,0 @@
-public class Livro {
-	public int Id { get; set; }
-	public string titulo { get; set; }
-	public Autor autor { get; set; }
-}
